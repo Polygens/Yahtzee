0b7852bd66d6be5cb5d033d75e94db3e53b4e889
M <marlies.geerts@student.kdg.be>
2015-10-18 14:47:19 +0200

    Tabs modified

--- Yahtzee/YahtzeeModel.cs
+++ Yahtzee/YahtzeeModel.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 namespace Yahtzee
 {
-  public class YahtzeeModel
-  {
-    private int aantalTeerlingen = 5; //, aantalWorpen = 0;
+	public class YahtzeeModel
+	{
+		private int aantalTeerlingen = 5; //, aantalWorpen = 0;
 
-    public int AantalTeerlingen {
-      get { return aantalTeerlingen; } 
-      set { aantalTeerlingen = value; } 
-    }
+		public int AantalTeerlingen {
+			get { return aantalTeerlingen; }
+			set { aantalTeerlingen = value; }
+		}
 
-    //public int AantalWorpen
-    //{
-    //  get { return aantalWorpen; }
-    //  set { aantalWorpen = value; }
-    //}  
+		//public int AantalWorpen
+		//{
+		//  get { return aantalWorpen; }
+		//  set { aantalWorpen = value; }
+		//}  
 
-  }
+	}
 }
