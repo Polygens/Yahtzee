972637c3011322769536e5469f3148078a2b8bfa
William87137 <willem.gillis@gmail.com>
2015-10-28 14:55:17 +0100

    Daphne fix

--- Yahtzee/TeerlingView.cs
+++ Yahtzee/TeerlingView.cs
@@ -36,15 +36,15 @@ namespace Yahtzee
 			}
 		}
 
-    public void AbleThrow()
-    {
-      controller.Losmaken();
-      TeerlingLabel.ForeColor = controller.model.KleurTeerling;
-      if (controller.model.isBtnVisible)
-      {
-        TeerlingWerpen.Show();
-      }
-    }
+		public void AbleThrow()
+		{
+			controller.Losmaken();
+			TeerlingLabel.ForeColor = controller.model.KleurTeerling;
+			if (controller.model.isBtnVisible)
+			{
+				TeerlingWerpen.Show();
+			}
+		}
 
 		private void TeerlingLabel_Click(object sender, EventArgs e)
 		{
@@ -56,58 +56,52 @@ namespace Yahtzee
 			label1.Text = "Teerling: " + (controller.model.IndexOfTeerling + 1);
 		}
 
+		//cheat knoppen verschijnen
+		public void MakeButtonsVisible()
+		{
+			nr1.Show();
+			nr2.Show();
+			nr3.Show();
+			nr4.Show();
+			nr5.Show();
+			nr6.Show();
+		}
 
-    //cheat knoppen verschijnen
-    public void MakeButtonsVisible()
-    {
+		// cheat knop waarde doorgeven
+		public void CheatButtonValue(int value)
+		{
+			TeerlingLabel.Text = value.ToString();
+			controller.model.AantalOgen = value;
+		}
 
-        nr1.Show();
-        nr2.Show();
-        nr3.Show();
-        nr4.Show();
-        nr5.Show();
-        nr6.Show();
-        
-    }
+		private void nr1_Click(object sender, EventArgs e)
+		{
+			CheatButtonValue(1);
+		}
 
-    // cheat knop waarde doorgeven
-    public void CheatButtonValue(Button btn)
-    {
-      TeerlingLabel.Text = btn.Text;
-      SetText();
-    
-    }
+		private void nr2_Click(object sender, EventArgs e)
+		{
+			CheatButtonValue(2);
+		}
 
-    private void nr1_Click(object sender, EventArgs e)
-    {
-      CheatButtonValue(nr1);
-    }
+		private void nr3_Click(object sender, EventArgs e)
+		{
+			CheatButtonValue(3);
+		}
 
-    private void nr2_Click(object sender, EventArgs e)
-    {
-      CheatButtonValue(nr2);
-    }
+		private void nr4_Click(object sender, EventArgs e)
+		{
+			CheatButtonValue(4);
+		}
 
-    private void nr3_Click(object sender, EventArgs e)
-    {
-      CheatButtonValue(nr3);
-    }
+		private void nr5_Click(object sender, EventArgs e)
+		{
+			CheatButtonValue(5);
+		}
 
-    private void nr4_Click(object sender, EventArgs e)
-    {
-      CheatButtonValue(nr4);
-    }
-
-    private void nr5_Click(object sender, EventArgs e)
-    {
-      CheatButtonValue(nr5);
-    }
-
-    private void nr6_Click(object sender, EventArgs e)
-    {
-      CheatButtonValue(nr6);
-    }
-
-
+		private void nr6_Click(object sender, EventArgs e)
+		{
+			CheatButtonValue(6);
+		}
 	}
 }
